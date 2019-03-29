fb87fa8af2bea5f284bd4bd8abc2f6b0e27517fa
clevmania <lawrence@tru-data.com>
2019-03-29 16:51:39 +0100

    update

--- resources/js/directory-listing.js
+++ resources/js/directory-listing.js
@@ -211,3 +211,55 @@ function returnfiltered(searchList){
     return searchList.includes(document.querySelector("#tested").value)
     // === document.querySelector("#tested").value.slice(0, 1).toUpperCase();
 }
+
+
+
+
+
+let headerInputContainer = document.querySelector(".header__form-input-container--tab")
+
+headerInputContainer.addEventListener("mouseover", function (e) {
+    
+    $(".header__form-input--tab").focus();
+    
+});
+
+
+let menuIcon = $(".header__menu_icon");
+let menuModal = $(".nav_menu__resp");
+
+menuIcon.click(function () {
+    menuModal.animate({ left: "0%" });
+    document.body.style.height = "100vh";
+    document.body.style.overflow = "hidden";
+    // document.querySelector(".menu_icon").style.visibility = "hidden";
+});
+
+$(window).click(function (e) {
+    if (e.target.className === "nav_menu__resp") {
+        //remv the modal
+        menuModal.animate({ left: "-100%" });
+        // height of the document
+        document.body.style.height = "auto";
+        //make the document scrollable again
+        document.body.style.overflow = "auto";
+        // document.querySelector(".menu_icon").style.visibility = "visible";
+    }
+});
+
+
+function colorRed() {
+
+    for (let i = 0; i < document.querySelectorAll(".resp_menu__link--active:link path").length; i++) {
+        document.querySelectorAll(".resp_menu__link--active:link path")[i].setAttribute("fill", "#dcbd56");
+        if (document.querySelectorAll(".resp_menu__link--active:link polygon").length > 0) {
+            document.querySelectorAll(".resp_menu__link--active:link polygon")[i].setAttribute("fill", "#dcbd56");
+        }
+        if (document.querySelectorAll(".resp_menu__link--active:link rect").length > 0) {
+            document.querySelectorAll(".resp_menu__link--active:link rect")[i].setAttribute("fill", "#dcbd56");
+        }
+    }
+
+}
+
+colorRed();
